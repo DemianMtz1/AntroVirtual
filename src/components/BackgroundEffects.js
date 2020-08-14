fb313a2e57b129e4b59d958570e659bd801711b2
Eric Demian Martinez Valdez <ericdemian.martinez@gmail.com>
2020-08-14 11:32:25 -0500

    Se agregan efectos de fondo sin funcionar

--- src/components/BackgroundEffects.js
+++ src/components/BackgroundEffects.js
@@ -7,9 +7,106 @@ const Bar = () => {
 
     return (
         <div>
-            <h3 >Saludos desde el background effects</h3>
-            <form className="form-group">
-                <input type="range"></input>
+            <h3 >Background effects</h3>
+            <form className="container form-group my-4">
+                
+                <div className="my-2">
+                    <p className="my-1">
+                        <label>Bartender working</label>
+                    </p>
+                    <div className="row justify-content-start">
+                        <div className="col-">
+                            <button>Play</button>
+                        </div>
+                        <div className="col-4">
+                            <input type="range"></input>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="my-2">
+                    <p className="my-1">
+                        <label>People talking</label>
+                    </p>
+                    <div className="row justify-content-start">
+                        <div className="col-">
+                            <button>Play</button>
+                        </div>
+                        <div className="col-4">
+                            <input type="range"></input>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="my-2">
+                    <p className="my-1">
+                        <label>Full room</label>
+                    </p>
+                    <div className="row justify-content-start">
+                        <div className="col-">
+                            <button>Play</button>
+                        </div>
+                        <div className="col-4">
+                            <input type="range"></input>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="my-2">
+                    <p className="my-1">
+                        <label>Rain on window</label>
+                    </p>
+                    <div className="row justify-content-start">
+                        <div className="col-">
+                            <button>Play</button>
+                        </div>
+                        <div className="col-4">
+                            <input type="range"></input>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="my-2">
+                    <p className="my-1">
+                        <label>Night ambiance</label>
+                    </p>
+                    <div className="row justify-content-start">
+                        <div className="col-">
+                            <button>Play</button>
+                        </div>
+                        <div className="col-4">
+                            <input type="range"></input>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="my-2">
+                    <p className="my-1">
+                        <label>Serving drinks</label>
+                    </p>
+                    <div className="row justify-content-start">
+                        <div className="col-">
+                            <button>Play</button>
+                        </div>
+                        <div className="col-4">
+                            <input type="range"></input>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="my-2">
+                    <p className="my-1">
+                        <label>Street ambience</label>
+                    </p>
+                    <div className="row justify-content-start">
+                        <div className="col-">
+                            <button>Play</button>
+                        </div>
+                        <div className="col-4">
+                            <input type="range"></input>
+                        </div>
+                    </div>
+                </div>
             </form>
         </div>
     )
