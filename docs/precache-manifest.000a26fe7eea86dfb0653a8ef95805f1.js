a09167d7c9bbb1b50eab996ca8c7b23ffd238abe
Angel V <angeelvallem@gmail.com>
2020-08-24 19:14:35 -0500

    haciendo el build

---
+++ docs/precache-manifest.000a26fe7eea86dfb0653a8ef95805f1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6954b4fdf3566116515751b9d883820d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb9df75182f2e40c311f",
+    "url": "/static/css/main.c32fc042.chunk.css"
+  },
+  {
+    "revision": "026e9c3f94ab81da17ea",
+    "url": "/static/js/2.fbea48fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fbea48fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9df75182f2e40c311f",
+    "url": "/static/js/main.90880150.chunk.js"
+  },
+  {
+    "revision": "9bac9ceedec46582fc91",
+    "url": "/static/js/runtime-main.79145da8.js"
+  },
+  {
+    "revision": "16c8964218806c4e459f0c125d21a259",
+    "url": "/static/media/BartenderWorking.16c89642.mp3"
+  },
+  {
+    "revision": "fded0d3b11f606163fbbac05ad94439c",
+    "url": "/static/media/N-ambience.fded0d3b.mp3"
+  },
+  {
+    "revision": "deb56f98242fcde5de5be24c39cb8524",
+    "url": "/static/media/ServingDrinks.deb56f98.mp3"
+  },
+  {
+    "revision": "6f295211149efbc6e78b7545ac0eccd1",
+    "url": "/static/media/people-talking.6f295211.mp3"
+  },
+  {
+    "revision": "bd3352101e44ba8fa440da1996cccac8",
+    "url": "/static/media/rainOnWindow.bd335210.mp3"
+  }
+]);
