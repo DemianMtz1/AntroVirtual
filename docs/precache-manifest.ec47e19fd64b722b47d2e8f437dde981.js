eecf1752450688093c0df49f7e3fbfc17fa3a697
Eric Demian Martinez Valdez <ericdemian.martinez@gmail.com>
2020-09-02 18:41:24 -0500

    Se soluciona bug x2

---
+++ docs/precache-manifest.ec47e19fd64b722b47d2e8f437dde981.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "570966a83fd440774c6529cbd3100897",
+    "url": "/AntroVirtual/index.html"
+  },
+  {
+    "revision": "297596591beaa531e61d",
+    "url": "/AntroVirtual/static/css/main.069b809a.chunk.css"
+  },
+  {
+    "revision": "91c78e3dd7c46a17273f",
+    "url": "/AntroVirtual/static/js/2.adcfc389.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AntroVirtual/static/js/2.adcfc389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297596591beaa531e61d",
+    "url": "/AntroVirtual/static/js/main.543c033e.chunk.js"
+  },
+  {
+    "revision": "789a21908e543f75fc19",
+    "url": "/AntroVirtual/static/js/runtime-main.3fad7408.js"
+  },
+  {
+    "revision": "16c8964218806c4e459f0c125d21a259",
+    "url": "/AntroVirtual/static/media/BartenderWorking.16c89642.mp3"
+  },
+  {
+    "revision": "fded0d3b11f606163fbbac05ad94439c",
+    "url": "/AntroVirtual/static/media/N-ambience.fded0d3b.mp3"
+  },
+  {
+    "revision": "deb56f98242fcde5de5be24c39cb8524",
+    "url": "/AntroVirtual/static/media/ServingDrinks.deb56f98.mp3"
+  },
+  {
+    "revision": "6f295211149efbc6e78b7545ac0eccd1",
+    "url": "/AntroVirtual/static/media/people-talking.6f295211.mp3"
+  },
+  {
+    "revision": "bd3352101e44ba8fa440da1996cccac8",
+    "url": "/AntroVirtual/static/media/rainOnWindow.bd335210.mp3"
+  }
+]);
